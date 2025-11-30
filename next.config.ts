761670449bdfa9d057e39545636d257eaaafc377
karlekartier <karthickkishoreking@gmail.com>
2025-11-30 23:49:22 +0530

    ntn

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
 
-const basePath = isProd ? "/App" : "";
+const basePath = isProd ? "/My-App" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
