a4276978f7895cbee9b6568a7dc0975898338ade
karlekartier <karthickkishoreking@gmail.com>
2025-12-01 00:01:33 +0530

    dfsdf

---
+++ .next/dev/static/chunks/src_app_not-found_tsx_0bb141b0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a8378300._.js",
+  "static/chunks/src_5b928f8a._.js"
+],
+    source: "dynamic"
+});
