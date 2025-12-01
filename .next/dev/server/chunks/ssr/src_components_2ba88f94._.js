90aa7ba6aff466be5d2409d3fb9acec7b92e5946
karlekartier <karthickkishoreking@gmail.com>
2025-12-01 11:55:18 +0530

    qwdd

--- .next/dev/server/chunks/ssr/src_components_2ba88f94._.js
+++ .next/dev/server/chunks/ssr/src_components_2ba88f94._.js
@@ -339,7 +339,7 @@ function Footer() {
                                     className: "flex items-center gap-4 mb-6",
                                     children: [
                                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"], {
-                                            src: "../assets/favicon/kk_logo.svg",
+                                            src: "/assets/favicon/kk_logo.svg",
                                             alt: "Logo",
                                             width: 40,
                                             height: 40,
