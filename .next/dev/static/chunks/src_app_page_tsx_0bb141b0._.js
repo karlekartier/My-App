220c324b02510dc48034913e786a04966c496221
karlekartier <karthickkishoreking@gmail.com>
2025-12-02 10:51:44 +0530

    fghdwefwwt43

--- .next/dev/static/chunks/src_app_page_tsx_0bb141b0._.js
+++ .next/dev/static/chunks/src_app_page_tsx_0bb141b0._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_fc37e4e2._.js",
-  "static/chunks/node_modules_450cbbc0._.js"
+  "static/chunks/src_9e7ab181._.js",
+  "static/chunks/node_modules_64952a0e._.js"
 ],
     source: "dynamic"
 });
